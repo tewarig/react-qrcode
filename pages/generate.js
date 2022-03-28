7cf5cd829b2eb573f549a52b0bc41cb6dd14b752
tewarig <gauravtewari111@gmail.com>
2022-03-28 18:57:35 +0530

    feat: qr code reader added

--- pages/generate.js
+++ pages/generate.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 import QRCode from "react-qr-code";
+import styles from "../styles/Home.module.css";
 
 function Generate() {
   const [qrCodeValue, setQrCodeValue] = useState("");
-  
 
   return (
-    <>
-      {qrCodeValue != "" && <QRCode value={qrCodeValue} />}
-      <input onChange={(e)=>{setQrCodeValue(e.target.value)}} /> 
-      <div>Generate</div>
-    </>
+    <div className={styles.main}>
+      {qrCodeValue != "" && (
+        <QRCode value={qrCodeValue} className={styles.containerColumn} />
+      )}
+      <input
+        className={styles.card}
+        onChange={(e) => {
+          setQrCodeValue(e.target.value);
+        }}
+      />
+      <div className={styles.card}>Generate &rarr;</div>
+    </div>
   );
 }
 
--- pages/scan.js
+++ pages/scan.js
@@ -1,25 +1,31 @@
 import React, { useState, useRef } from "react";
 import { QrReader } from "react-qr-reader";
+import styles from "../styles/Home.module.css";
 
 function Scan() {
   const [data, setData] = useState("No result");
 
   return (
-    <>
-      <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-          }
+    <div className={styles.container}>
+      <div className={styles.container}>
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.text);
+            }
 
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        style={{ width: "40%", height: "40%" }}
-      />
-      <p>{data}</p>
-    </>
+            if (!!error) {
+              console.info(error);
+            }
+            
+          } 
+        }
+        constraints	={{ facingMode:  "environment"  }}
+          style={{ width: "40%", height: "40%" }}
+        />
+        <p>{data}</p>
+      </div>
+    </div>
   );
 }
 
