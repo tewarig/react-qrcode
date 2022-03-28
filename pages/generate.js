d4a6fed6e044f92102155ea1521ef5f247ab2556
tewarig <gauravtewari111@gmail.com>
2022-03-28 17:43:37 +0530

    feat: generate qr code added

--- pages/generate.js
+++ pages/generate.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
 
 function Generate() {
+  const [qrCodeValue, setQrCodeValue] = useState("");
+  
+
   return (
-    <div>Generate</div>
-  )
+    <>
+      {qrCodeValue != "" && <QRCode value={qrCodeValue} />}
+      <input onChange={(e)=>{setQrCodeValue(e.target.value)}} /> 
+      <div>Generate</div>
+    </>
+  );
 }
 
-export default Generate
+export default Generate;
